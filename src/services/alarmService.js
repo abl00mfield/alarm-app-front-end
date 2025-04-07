695cad5085a621d9dd0199b3775afac526404669
abl00mfield <amanda.bloomfield@gmail.com>
2025-04-07 12:26:23 -0600

    wrote index, create, and get service functions for alarms

---
+++ src/services/alarmService.js
@@ -0,0 +1,45 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/alarms`;
+
+const index = async () => {
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    return res.json();
+  } catch (err) {
+    console.error(error);
+  }
+};
+
+//POST creating a new alarm /alarms
+const create = async (alarmFormData) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(alarmFormData),
+    });
+    return res.json();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+//GET show route, show a single alarm
+const show = async (alarmId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${alarmId}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    return res.json();
+  } catch (err) {
+    console.error(error);
+  }
+};
