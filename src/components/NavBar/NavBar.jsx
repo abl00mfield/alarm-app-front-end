4ad06ef424dc2c9c7a7fc47ae4e2cc841be0f5a6
abl00mfield <amanda.bloomfield@gmail.com>
2025-04-09 10:45:59 -0600

    added fragment back to NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { Link } from "react-router";
 
 import { UserContext } from "../../contexts/UserContext";
-import styles from './NavBar.module.css';
+import styles from "./NavBar.module.css";
 
 const NavBar = () => {
   const { user, setUser } = useContext(UserContext);
@@ -13,48 +13,47 @@ const NavBar = () => {
   };
 
   return (
-
-    <nav className={styles.container}>
-
-      {user ? (
-        <ul>
-          <li>APP NAME</li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/alarms">Your Alarms</Link>
-          </li>
-          <li>
-            <Link to="alarms/new">Create a new Alarm</Link>
-          </li>
-          <li>
-            <Link to="/" onClick={handleSignOut}>
-              Sign Out
-            </Link>
-          </li>
-        </ul>
-      ) : (
-        <ul>
-          <div className={styles.left}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          </div>
-            <div className={styles.right}>
-          <li>
-            <Link to="/sign-in">Sign In</Link>
-          </li>
-          <li>
-            <Link to="/sign-up">Sign Up</Link>
-          </li>
+    <>
+      <nav className={styles.container}>
+        {user ? (
+          <ul>
+            <li>APP NAME</li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/alarms">Your Alarms</Link>
+            </li>
+            <li>
+              <Link to="alarms/new">Create a new Alarm</Link>
+            </li>
+            <li>
+              <Link to="/" onClick={handleSignOut}>
+                Sign Out
+              </Link>
+            </li>
+          </ul>
+        ) : (
+          <ul>
+            <div className={styles.left}>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
             </div>
-        </ul>
-      )}
-    </nav>
-    <div>
-      <button className='themeBtn'>Change Theme</button>
-    </div>
+            <div className={styles.right}>
+              <li>
+                <Link to="/sign-in">Sign In</Link>
+              </li>
+              <li>
+                <Link to="/sign-up">Sign Up</Link>
+              </li>
+            </div>
+          </ul>
+        )}
+      </nav>
+      <div>
+        <button className="themeBtn">Change Theme</button>
+      </div>
     </>
   );
 };
