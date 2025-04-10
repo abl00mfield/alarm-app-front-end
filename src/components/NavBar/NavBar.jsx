1f3e1688998bf399d5eb1b186aace41f29a803e7
celisawalker <celisa.walker@gmail.com>
2025-04-10 13:34:14 -0700

    corrected return statement in navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,49 +13,43 @@ const NavBar = () => {
   };
 
   return (
-
     <nav className={styles.container}>
-
       {user ? (
-        <ul>
-          <li>APP NAME</li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/alarms">Your Alarms</Link>
-          </li>
-          <li>
-            <Link to="alarms/new">Create a new Alarm</Link>
-          </li>
-          <li>
-            <Link to="/" onClick={handleSignOut}>
-              Sign Out
-            </Link>
-          </li>
-        </ul>
+        <div className={styles.navContainer}>
+          <ul>
+            <li>Alarm Mate</li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/alarms">Your Alarms</Link>
+            </li>
+            <li>
+              <Link to="/" onClick={handleSignOut}>
+                Sign Out
+              </Link>
+            </li>
+          </ul>
+          <ThemeToggle />
+        </div>
       ) : (
         <ul>
           <div className={styles.left}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
           </div>
-            <div className={styles.right}>
-          <li>
-            <Link to="/sign-in">Sign In</Link>
-          </li>
-          <li>
-            <Link to="/sign-up">Sign Up</Link>
-          </li>
-            </div>
+          <div className={styles.right}>
+            <li>
+              <Link to="/sign-in">Sign In</Link>
+            </li>
+            <li>
+              <Link to="/sign-up">Sign Up</Link>
+            </li>
+          </div>
         </ul>
       )}
     </nav>
-    <div>
-      <button className='themeBtn'>Change Theme</button>
-    </div>
-    </>
   );
 };
 
